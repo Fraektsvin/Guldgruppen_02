7fa7b15823c91beab4920f431859a5ecf0c8b5b9
SigurdEEspersen <32567367+SigurdEEspersen@users.noreply.github.com>
2017-11-27 16:50:29 +0100

    Merge conflicts

--- src/Acquaintance/IRoom.java
+++ src/Acquaintance/IRoom.java
@@ -10,11 +10,18 @@ package Acquaintance;
  * @author sigur
  */
 public interface IRoom {
+
     String getExitString();
+
     IRoom getExit(String direction);
+
     String getLongDescription();
+
     String getMediumDescription();
+
     ICoin getCoin(String CoinName);
+
     void removeCoin(String CoinName);
+
     boolean isLocked(String direction);
 }
